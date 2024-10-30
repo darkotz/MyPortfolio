254ee19bd79d4152ecc5819de921b64a10b09757
sypherr <nikita_sr@icloud.com>
2024-10-30 21:30:49 +0100

    123
    123

--- MyPortfolio/src/header.jsx
+++ MyPortfolio/src/header.jsx
@@ -1,9 +1,25 @@
 import "./header.css";
 import { useState } from "react";
-import { useEffect } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 export default function Header() {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -23,7 +39,15 @@ export default function Header() {
       <div className="btnGroup">
         <button>Contact me</button>
         <button>Info</button>
-        <button>Meny</button>
+        <button onClick={toggleDrawer(anchor, true)}>Menu</button>
+
+        <Drawer
+          anchor={anchor}
+          open={state[anchor]}
+          onClose={toggleDrawer(anchor, false)}
+        >
+          {list(anchor)}
+        </Drawer>
       </div>
     </div>
   );
